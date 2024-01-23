536165f9dfc304eb5bb5a0aad53eb2f4457955b9
joseantoniorbieshna <jaramosb05@iesarroyoharnina.es>
2024-01-23 09:03:10 +0100

    Init

---
+++ apimovil/src/main/java/com/apimovil/ApimovilApplication.java
@@ -0,0 +1,13 @@
+package com.apimovil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimovilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimovilApplication.class, args);
+	}
+
+}
---
+++ apimovil/src/test/java/com/apimovil/ApimovilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apimovil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimovilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
