0a5f0fbfb85db7dab2ced41a6c7b913f1ac3e0a5
ManuCB1 <139983471+ManuCB1@users.noreply.github.com>
2024-01-26 20:18:18 +0100

    issue#08
    Creación Entidad TecnologiaPantalla, su repo y su test

--- apimovil/src/main/java/com/apimovil/models/entities/Movil.java
+++ apimovil/src/main/java/com/apimovil/models/entities/Movil.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
@@ -44,7 +45,8 @@ public class Movil {
 	//private Pantalla pantalla;
 	//private Modelo modelo;
 	//private TamanoPantalla tamanoPantalla;
-	//private TecnologiaPantalla tecnologiaPantalla;
+	@ManyToOne
+	private TecnologiaPantalla tecnologiaPantalla;
 	private int almacenamiento;
 	private double ram;
 	private double peso;
