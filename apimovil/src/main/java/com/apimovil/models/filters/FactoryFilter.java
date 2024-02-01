75da0e928fd73140e55de3c3bc48ce45889f2fbc
ManuCB1 <139983471+ManuCB1@users.noreply.github.com>
2024-02-01 20:12:10 +0100

    issue#18
    Creación Mocks

--- apimovil/src/main/java/com/apimovil/models/filters/FactoryFilter.java
+++ apimovil/src/main/java/com/apimovil/models/filters/FactoryFilter.java
@@ -3,7 +3,10 @@ package com.apimovil.models.filters;
 import java.util.List;
 
 public enum FactoryFilter {
-	ALL(List.of(new PrecioFilter(),new FilterRam()));
+	ALL(List.of(new PrecioFilter(),new FilterRam(), new FilterAlmacenamiento(), 
+			new FilterBateria(), new FilterCamara(), new FilterMarca(), 
+			new FilterModelo(), new FilterScreenSize(), new FilterTecnologiaPantalla(),
+			new FilterVisita(), new NFCFilter(), new PesoFilter(), new ProcesadorFilter()));
 	private List<IFilter> filters;
 	private FactoryFilter(List<IFilter> filters) {
 		this.filters=filters;
