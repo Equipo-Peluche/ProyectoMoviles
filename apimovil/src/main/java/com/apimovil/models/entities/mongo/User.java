d5333c3cd4fcdb5874a16b2439fe428030ca3336
jesusmariaieshna <jmrodriguezp11@iesarroyoharnina.es>
2024-02-28 13:06:27 +0100

    Issue#25
    Creando la clase abstracta petición

--- apimovil/src/main/java/com/apimovil/models/entities/mongo/User.java
+++ apimovil/src/main/java/com/apimovil/models/entities/mongo/User.java
@@ -36,7 +36,6 @@ public class User {
 	@Indexed(unique = true, background = true)
 	private String username;
 	@NotBlank(message = "no puede estar en blanco")
-//	@Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d;,\\.]{8,}$", message = "Debe tener al menos 8 caracteres, contener al menos una mayúscula, una minúscula, un número y un símbolo entre coma, punto y coma, o punto")
 	private String password;
 
 
