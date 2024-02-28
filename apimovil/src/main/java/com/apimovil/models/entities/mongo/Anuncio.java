ddf7063788e01446bc8c7b3774800d8f0aa360c7
felixieshna <117735972+felixieshna@users.noreply.github.com>
2024-02-28 13:05:39 +0100

    abstract Anuncio

---
+++ apimovil/src/main/java/com/apimovil/models/entities/mongo/Anuncio.java
@@ -0,0 +1,27 @@
+package com.apimovil.models.entities.mongo;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public abstract class Anuncio {
+
+	@Id
+	@Field("_id")
+	private ObjectId idAnuncio;
+	@Field ("usuario")
+	
+	private User user;
+	
+	private String marca;
+	private String modelo;
+	
+}
